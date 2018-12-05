339551d6e6eca3144188352a8279a6d264c63c46
samuba <samuel.z.bach@gmail.com>
2018-12-06 00:30:06 +0100

    1

--- main.go
+++ main.go
@@ -1,32 +1,40 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"io"
-	"io/ioutil"
 	"os/exec"
 	"strings"
+	"time"
 )
 
 func main() {
 	fmt.Println("parsing eventlogs...")
-
-	cmdArgs := strings.Fields("wevtutil qe System /c:10")
+	cmdArgs := strings.Fields("wevtutil qe System /c:100")
 	cmd := exec.Command(cmdArgs[0], cmdArgs[1:len(cmdArgs)]...)
-	stdout, _ := cmd.StdoutPipe()
-	cmd.Start()
-	go print(stdout)
-	cmd.Wait()
-}
 
-// to print the processed information when stdout gets a new line
-func print(stdout io.ReadCloser) {
-	bytea, err := ioutil.ReadAll(stdout)
+	bytea, err := cmd.CombinedOutput()
 	if err != nil {
 		fmt.Println("error: ", err)
 	}
-	line := string(bytea[:])
+
+	lines := strings.Split(string(bytea[:len(bytea)-1]), "\n")
+
+	for _, line := range lines {
+		tim, _ := exctractTime(line)
+		fmt.Println(tim)
+	}
+
+	fmt.Println(len(lines))
+}
+
+func exctractTime(str string) (tim time.Time, err error) {
 	timepattern := "TimeCreated SystemTime='"
-	start := strings.Index(line, timepattern)
-	fmt.Println("line: ", line[start+len(timepattern):start+len(timepattern)+30])
+	start := strings.Index(str, timepattern)
+	if start <= 0 {
+		return time.Now(), errors.New("no time found")
+	}
+	timeStr := str[start+len(timepattern) : start+len(timepattern)+30]
+	timeStr = timeStr[:len(timeStr)-7] + "Z"
+	return time.Parse("2006-01-02T15:04:05.000Z", timeStr)
 }
